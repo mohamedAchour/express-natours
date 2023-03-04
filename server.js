490f0abb11f454b11de58846a99b0be860eba652
mohamedAchour <mohamedachour421@gmail.com>
2023-03-04 15:32:58 +0100

    add mongoose connection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 //read the env file before requiring the app
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+mongoose.connect();
 //###########START THE SERVER###########
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
