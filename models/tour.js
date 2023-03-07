88bed1517d6612a49d66efa7ebef672b48d2013f
mohamedAchour <mohamedachour421@gmail.com>
2023-03-07 14:25:52 +0100

    add tour schema

--- models/tour.js
+++ models/tour.js
@@ -1,14 +1,51 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingAverage: { type: Number, default: 4.5 },
+    ratingQuantity: { type: Number, default: 0 },
+    price: { type: Number, required: [true, 'A tour must have a price'] },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, ' tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    //it is a good practice to store images in the fs not in db, here we are just giving a name
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+
+    images: [String],
+    startDates: [Date],
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-});
+  { timestamps: true }
+);
 
 const Tour = mongoose.model('Tour', tourSchema);
 
