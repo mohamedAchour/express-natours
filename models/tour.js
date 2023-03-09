b86bae7a1a4f5f449e3ca59ae75fe7a538eefef6
mohamedAchour <mohamedachour421@gmail.com>
2023-03-09 17:51:10 +0100

    fix schemas fields

--- models/tour.js
+++ models/tour.js
@@ -20,8 +20,8 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a difficulty'],
     },
-    ratingAverage: { type: Number, default: 4.5 },
-    ratingQuantity: { type: Number, default: 0 },
+    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
     priceDiscount: {
       type: Number,
